d7591094ce30cc107aacb235c060e27f97c34353
dev-swi2021 <77826604+dev-swi2021@users.noreply.github.com>
2021-03-02 16:09:37 +0900

    Update dataset.py
    _process func 변경

--- Prediction/MyLotto/dataset.py
+++ Prediction/MyLotto/dataset.py
@@ -23,23 +23,26 @@ class CustomDataset(Dataset):
   
   def __getitem__(self, idx):
     feature = torch.from_numpy(self.feature[idx]).float()
-    target = torch.from_numpy(self.target[idx]).float()
+    target = torch.from_numpy(self.target[idx]).float() if self.train else None
     return feature, target
   
+  def _transform_one_hot_encode(self, numbers):
+      number_lst = [0]*45 # 0->1, 44->45
+      for n in numbers:
+        number_lst[n-1] = 1
+      return np.array(number_lst)
+    
   def _process(self):
     """
       Feature 추가 해야함
     """
-    
-    last = self.data.shape[0] - (self.k - 1)
-    one_hot_encode = 45
-    
-    target = self.data[self.data.columns[pd.Series(self.data.columns).str(startswith('다음번호')]]
-    feature = self.data.drop(columns=self.data.columns[pd.Series(self.data.columns).str.startswith('다음번호')])
-    
-    # transform k data --> one data
-    feature_k_lst = [feature[i : i+self.k].reset_index(drop=True).to_numpy().reshape(-1,self.k*feature.shape[1]) for i in range(last_)]
-    target_k_lst = [np.eye(one_hot_encode)[target.iloc[i].to_numpy()] for i in range(self.k-1, last_)] if self.train else None  # 각 데이터마다 (6,46) 크기
-    # target_k_lst = [target.iloc[i].to_numpy() for i in range(self.k-1, last_)] if self.train else None  # 각 데이터마다 (6,46) 크기
-    return feature_k_lst, target_k_lst
-    
+    last_ = self.data.shape[0] - self.k
+    tmp_data = self.data.drop(columns=['회차'])
+    feature_k_lst = [tmp_data.iloc[idx:idx+self.k].to_numpy().reshape(-1, self.k*tmp_data.shape[1]) for idx in range(last_)]
+    target_k_lst = [self._transform_one_hot_encode(tmp_data.loc[idx+self.k, '번호_1':'번호_6'].to_numpy()) for idx in range(last_)] if self.train else None
+    for idx in range(last_):
+      feature_k_lst.append(tmp_data.iloc[idx:idx+self.k].to_numpy().reshape(-1,self.k*tmp_data.shape[1]))
+      if self.train:
+        # 정답 데이터 one-hot으로 변경
+        target_k_lst.append(self._transform_one_hot_encode(tmp_data.loc[idx+self.k, '번호_1':'번호_6'].to_numpy()))  # 0->1, 44->45
+    return feature_k_lst, target_k_lst    
